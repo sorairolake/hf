edd65d7e0425eeaac87a52b92a4cb775c983de62
Shun Sakai <sorairolake@protonmail.ch>
2022-09-15 16:28:18 +0900

    Cargo.toml: Update dependencies

--- src/windows.rs
+++ src/windows.rs
@@ -4,52 +4,55 @@
 // Copyright (C) 2022 Shun Sakai
 //
 
+use std::ffi::CString;
 use std::fs;
 use std::io;
 use std::os::windows::fs::MetadataExt;
 use std::path::Path;
 
-use windows::Win32::Storage::FileSystem::{
-    SetFileAttributesA, FILE_ATTRIBUTE_HIDDEN, FILE_FLAGS_AND_ATTRIBUTES,
-};
+use windows::{core::PCSTR, Win32::Storage::FileSystem};
 
-fn get_file_attributes(path: impl AsRef<Path>) -> io::Result<FILE_FLAGS_AND_ATTRIBUTES> {
+fn get_file_attributes(
+    path: impl AsRef<Path>,
+) -> io::Result<FileSystem::FILE_FLAGS_AND_ATTRIBUTES> {
     let attributes = fs::metadata(path.as_ref())?.file_attributes();
 
-    Ok(FILE_FLAGS_AND_ATTRIBUTES(attributes))
+    Ok(FileSystem::FILE_FLAGS_AND_ATTRIBUTES(attributes))
 }
 
 /// Returns `true` if the file is a hidden file.
 pub fn is_hidden(path: impl AsRef<Path>) -> io::Result<bool> {
     let attributes = get_file_attributes(path)?;
 
-    Ok((attributes & FILE_ATTRIBUTE_HIDDEN).0 > 0)
+    Ok((attributes & FileSystem::FILE_ATTRIBUTE_HIDDEN).0 > 0)
 }
 
 /// Hide a file or directory.
 pub fn hide(path: impl AsRef<Path>) -> io::Result<()> {
-    let path = path
-        .as_ref()
-        .to_str()
-        .ok_or_else(|| io::Error::from(io::ErrorKind::InvalidData))?;
+    let path = path.as_ref().to_str().unwrap_or_default();
 
-    let attributes = get_file_attributes(path)? | FILE_ATTRIBUTE_HIDDEN;
+    let attributes = get_file_attributes(path)? | FileSystem::FILE_ATTRIBUTE_HIDDEN;
 
-    unsafe { SetFileAttributesA(path, attributes) }
+    let path = CString::new(path).map_err(io::Error::from)?;
+    let bytes = path.as_bytes_with_nul();
+    let path = PCSTR::from_raw(bytes.as_ptr());
+
+    unsafe { FileSystem::SetFileAttributesA(path, attributes) }
         .ok()
         .map_err(io::Error::from)
 }
 
 /// Show a hidden file or hidden directory.
 pub fn show(path: impl AsRef<Path>) -> io::Result<()> {
-    let path = path
-        .as_ref()
-        .to_str()
-        .ok_or_else(|| io::Error::from(io::ErrorKind::InvalidData))?;
+    let path = path.as_ref().to_str().unwrap_or_default();
 
-    let attributes = get_file_attributes(path)? & !FILE_ATTRIBUTE_HIDDEN;
+    let attributes = get_file_attributes(path)? & !FileSystem::FILE_ATTRIBUTE_HIDDEN;
 
-    unsafe { SetFileAttributesA(path, attributes) }
+    let path = CString::new(path).map_err(io::Error::from)?;
+    let bytes = path.as_bytes_with_nul();
+    let path = PCSTR::from_raw(bytes.as_ptr());
+
+    unsafe { FileSystem::SetFileAttributesA(path, attributes) }
         .ok()
         .map_err(io::Error::from)
 }
