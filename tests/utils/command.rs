640c74064872157ba743c824c0c44f09c7ff3d2d
Shun Sakai <sorairolake@protonmail.ch>
2024-05-21 07:50:58 +0900

    Update `command.rs`

--- tests/utils/command.rs
+++ tests/utils/command.rs
@@ -2,8 +2,10 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-pub fn command() -> assert_cmd::Command {
-    let mut command = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+use assert_cmd::Command;
+
+pub fn command() -> Command {
+    let mut command = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     command.current_dir("tests");
     command
 }
